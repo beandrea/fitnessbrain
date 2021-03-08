14d82c4d5f09824651e748c78a307df601fc2fcf
Andrea <arkflame99@gmail.com>
2021-03-07 19:48:49 -0500

    working?

--- models/workout.js
+++ models/workout.js
@@ -1,11 +1,52 @@
-import React from 'react'
+const mongoose = require("mongoose");
 
-function workout() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Schema = mongoose.Schema;
 
-export default workout;
+const WorkoutSchema = new Schema({
+
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+
+      name: {
+        type: String,
+      },
+
+      duration: {
+        type: Number
+      },
+
+      weight: {
+        type: Number
+      },
+
+      reps: {
+        type: Number
+      },
+
+      sets: {
+        type: Number
+      },
+
+      distance: {
+        type: Number
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
